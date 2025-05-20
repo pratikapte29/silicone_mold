af514b0d2b13fc9e1ddc71613d4d036be47e8401
pratik <pratikapte2911@gmail.com>
2025-05-20 13:15:54 +0530

    Finish Candidate Draw Direction determination using rendering

---
+++ main.py
@@ -0,0 +1,15 @@
+from src.finalize_draw_direction import FinalizeDrawDirection
+import time
+
+start_time = time.time()
+mesh_path = "assets/stl/xyzrgb_dragon.stl"
+num_vectors = 1000
+
+fd = FinalizeDrawDirection(mesh_path, num_vectors)
+
+candidate_vectors = fd.createCandidateVectors()
+
+draw_direction = fd.computeVisibleAreas(candidate_vectors)
+end_time = time.time()
+print(draw_direction)
+print(f"Time required for {num_vectors} candidates is {end_time - start_time:.2f} seconds")
