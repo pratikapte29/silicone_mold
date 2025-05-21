f8f0f95d509487bf1d875f5bf815d37646215b4b
pratik <pratikapte2911@gmail.com>
2025-05-21 13:02:47 +0530

    Add function to offset_stl

---
+++ unit_tests/offset_surface.py
@@ -0,0 +1,13 @@
+from src.split_mesh import offset_stl
+import numpy as np
+import pyvista as pv
+
+file_path = r"..\assets\stl\lucy.stl"
+
+pv_mesh, offset_mesh = offset_stl(file_path, 20)
+
+plotter = pv.Plotter()
+plotter.add_mesh(pv_mesh, color="lightgray", label="Original")
+plotter.add_mesh(offset_mesh, color="red", opacity=0.5, label="Offset")
+plotter.add_legend()
+plotter.show()
