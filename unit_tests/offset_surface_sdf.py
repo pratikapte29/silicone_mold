843e023d026fc4fdac4aa7c50515802572f294c5
pratik <pratikapte2911@gmail.com>
2025-05-23 10:16:07 +0530

    Update offset_surface_sdf calculation - working

--- unit_tests/offset_surface_sdf.py
+++ unit_tests/offset_surface_sdf.py
@@ -5,11 +5,11 @@ closedMesh = mrmeshpy.loadMesh(r"..\assets\stl\lucy.stl")
 
 # setup offset parameters
 params = mrmeshpy.OffsetParameters()
-params.voxelSize = 0.04
+params.voxelSize = 10
 # params.type = mrmeshpy.OffsetParametersType.Offset  # requires closed mesh
 
 # create positive offset mesh
-posOffset = mrmeshpy.offsetMesh(closedMesh, 0.2, params)
+posOffset = mrmeshpy.offsetMesh(closedMesh, 20, params)
 
 # save results
 mrmeshpy.saveMesh(posOffset, "posOffset.stl")
