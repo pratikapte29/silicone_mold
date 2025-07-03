94c51a1aa02b1fe5d171e63caab8847366b45ae0
pratikapte29 <pratikapte2911@gmail.com>
2025-07-03 09:00:41 +0530

    Update metamold halves generation

--- main.py
+++ main.py
@@ -123,5 +123,5 @@ ruledSurface(
 combined_mesh_path = os.path.join(results_dir, "combined_parting_surface.stl")
 
 generate_metamold(
-    combined_mesh_path, merged_blue_path, draw_direction
+    combined_mesh_path, merged_red_path, draw_direction
 )
